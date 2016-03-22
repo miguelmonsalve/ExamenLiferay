821bb5ef1d4aec7c512d0c2554fced0d7ab0012a
Arranque 1 <Arranque 1>
2016-03-22 10:51:11 +0100

    Implementamos la clase Java del controlador (UsersTable.java)

--- src/main/java/com/userstable/controlador/UsersTable.java
+++ src/main/java/com/userstable/controlador/UsersTable.java
@@ -1,11 +1,39 @@
 package com.userstable.controlador;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
-/**
- * Portlet implementation class UsersTable
- */
 public class UsersTable extends MVCPortlet {
- 
+
+	@Override
+	public void doView(RenderRequest renderRequest, RenderResponse renderResponse)
+			throws IOException, PortletException {
+
+		try {
+			List<User> usersList = UserLocalServiceUtil.getUsers(0, UserLocalServiceUtil.getUsersCount());
+
+			renderRequest.setAttribute("usuarios", usersList);
+
+			include("/html/userstable/view.jsp", renderRequest, renderResponse);
+
+		} catch (
+
+		SystemException e)
+
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 }
