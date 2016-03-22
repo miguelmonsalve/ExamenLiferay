f8a4edf6411da6969aa1518be1452d84997c280e
Arranque 1 <Arranque 1>
2016-03-22 10:47:09 +0100

    Creamos un Liferay Portlet

---
+++ src/main/java/com/userstable/controlador/UsersTable.java
@@ -0,0 +1,11 @@
+package com.userstable.controlador;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class UsersTable
+ */
+public class UsersTable extends MVCPortlet {
+ 
+
+}
